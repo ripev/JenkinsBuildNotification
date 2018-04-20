f4fc7742309ea7634c4415018f77ab2c118ce494
Andrey Makovetsky <makovetskiy.a@zoloto585.ru>
2018-04-20 18:01:59 +0300

    change wget to curl fror getting json from Jenkins

--- getjobs/getjobs.ps1
+++ getjobs/getjobs.ps1
@@ -12,28 +12,26 @@ $logo = "$($ScriptsFolderLocation)\jenkinsLogo.png"
 $JobsLogFile = "$($ScriptsFolderLocation)\JobsLog.log"
 $dateSuffix = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
 
-$jobsJsonOutput = "jobs.json"
-$buildNumberJsonOutput = "buildNumberJsonOutput.json"
 $NoActiveJobsMessage = "NoActiveJobs"
 
-$sh = "wget -q --no-check-certificate --auth-no-challenge --user $($user) --password $($pass) '$($jenkinsUri)/job/$($jenkisnFolderOrView)/view/All/api/json?tree=jobs[name,color]' -O $($jobsJsonOutput) > /dev/null"
+$sh = "curl -s -u $($user):$($pass) -k $($jenkinsUri)/job/$($jenkisnFolderOrView)/view/All/api/json?pretty"
 if ((Test-Path $JobsLogFile -ErrorAction SilentlyContinue) -eq $false) {
 	Write-Output "$($dateSuffix)`n$($NoActiveJobsMessage)" | Out-File $JobsLogFile -Append -Encoding utf8
 }
 $LastRunningJob = Get-Content -Path $JobsLogFile | Select-Object -Last 1
-bash -c $sh
-$jobsJson = Get-Content -Encoding UTF8 -Path $jobsJsonOutput | ConvertFrom-Json
-$jobRunning = ($jobsJson.jobs | Where-Object {$_.color -match "_anime"} | Select-Object name,color).name
+$output = bash -c $sh
+$jobsJson = $output | ConvertFrom-Json
+$jobRunning = ($jobsJson.jobs | Where-Object {$_.color -match "_anime"}).name
 if ($jobRunning.count -gt 0 -and $jobRunning -notmatch $LastRunningJob) {
-	$shBuildN = "wget -q --no-check-certificate --auth-no-challenge --user $($user) --password $($pass) '$($jenkinsUri)/job/$($jenkisnFolderOrView)/job/$($jobRunning)/api/json?tree=builds[number]' -O $($buildNumberJsonOutput) > /dev/null"
-	bash -c $shBuildN
-	$buildNumberJson = Get-Content -Encoding UTF8 -Path $buildNumberJsonOutput | ConvertFrom-Json
+	$shBuildN = "curl -s -u $($user):$($pass) -k $($jenkinsUri)/job/$($jenkisnFolderOrView)/job/$($jobRunning)/api/json?pretty"
+	$output = bash -c $shBuildN
+	$buildNumberJson = $output | ConvertFrom-Json
 	$lastBuildNumber = ($buildNumberJson.builds | Sort-Object number -Descending | Select-Object -First 1).number
 	$BlogButton = New-BTButton -Content 'View Log' -Arguments "$($jenkinsUri)/job/$($jenkisnFolderOrView)/job/$($jobRunning)/$($lastBuildNumber)/console"
 
-	$shBuildStarter = "wget -q --no-check-certificate --auth-no-challenge --user $($user) --password $($pass) '$($jenkinsUri)/job/$($jenkisnFolderOrView)/job/$($jobRunning)/$($lastBuildNumber)/api/json?pretty=true' -O $($buildNumberJsonOutput) > /dev/null"
-	bash -c $shBuildStarter
-	$buildStarterJson = Get-Content -Encoding UTF8 -Path $buildNumberJsonOutput | ConvertFrom-Json
+	$shBuildStarter = "curl -s -u $($user):$($pass) -k $($jenkinsUri)/job/$($jenkisnFolderOrView)/job/$($jobRunning)/$($lastBuildNumber)/api/json?pretty=true"
+	$output = bash -c $shBuildStarter
+	$buildStarterJson = $output | ConvertFrom-Json
 	$buildStarterName = (($buildStarterJson.actions | Where-Object {$_._class -eq "hudson.model.CauseAction"}).causes).shortDescription | Select-Object -First 1
 
 	New-BurntToastNotification -Text $jobRunning,$buildStarterName -Button $BlogButton -AppLogo $logo
@@ -43,6 +41,5 @@ if ($jobRunning.count -gt 0 -and $jobRunning -notmatch $LastRunningJob) {
 		Write-Output "`n$($dateSuffix)`n$($NoActiveJobsMessage)" | Out-File $JobsLogFile -Append -Encoding utf8
 	}
 }
-if ((Test-Path $jobsJsonOutput -ErrorAction SilentlyContinue) -eq $true) {Remove-Item $jobsJsonOutput -Force}
-if ((Test-Path $buildNumberJsonOutput -ErrorAction SilentlyContinue) -eq $true) {Remove-Item $buildNumberJsonOutput -Force}
+
 Set-Location -Path $currentLocation
